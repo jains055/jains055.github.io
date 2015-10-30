eab41fa484415a9cffa4c72dc3cb7ff6038aceed
jains055 <jains055@newschool.edu>
2015-10-30 16:33:12 +0000

    hksf

--- js1.js
+++ js1.js
@@ -1,17 +1,17 @@
 
 console.log("");
 
-var symbols = ["&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670; ", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670; ", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670; ", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670; ", "&#9670;", "&#9670;", "&#9670;", "&#9670;", "&#9670; ", "&#9670;", "&#9670;", "&#9670;"]
+var symbols = ["7", "#", "*", "0", "1", "&", "*", "# ", "+", "=", "^", "%", "$ ", "&", "/", "5", "9", "2 ", "!", "`", "~", "0", "3 ", "7", "7", "7",]
 
-var pattern = "&#8226;";
+var pattern = "7";
 
 for (var increment = 0; increment < symbols.length; increment++) {
 console.log(pattern);
 pattern = pattern + symbols[increment];
 var ampersand = "";
 console.log(increment);
-if (increment > 30) {
-for (var another = 0; another < 20; another++) {
+if (increment > 25) {
+for (var another = 0; another < 16; another++) {
 console.log(ampersand);
 ampersand = ampersand + ["-" , "_"]
 }
